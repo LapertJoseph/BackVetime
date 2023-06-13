7a3be18c695f309426b641166f3f56f2eb679eb3
LapertJoseph <lapertjoseph@gmail.com>
2023-06-13 12:07:15 +0200

    modif  query login return user info + isVeto

--- controllers/user/login.js
+++ controllers/user/login.js
@@ -7,19 +7,17 @@ const pool = require('../../config/database');
 module.exports = async (req, res) => {
     let connection;
     const { email, password } = req.query;
-    console.log(email)
     try {
         connection = await pool.getConnection();
         const result = await connection.query('CALL login(?, ?);', [email, password]);
-        console.log("result = ", result);
         if (result.length === 0) {
             return res.status(401).json({ success: false, data: "compte inconnu" });
         }
-        const data = result[0]
+        const data = result[0][0]
         req.session.uid = data.id;
         req.session.email = data.email;
-        console.log(result);
-        return res.status(200).json({ success: true, user: data })
+        console.log("data = ", data);
+        return res.status(200).json({ success: true, user: data})
     } catch (error) {
         return res.status(400).json({ error: error.message });
     } finally {
