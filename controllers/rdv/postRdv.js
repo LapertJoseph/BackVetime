cf732c2cd6669b38a668723aec0d4c4dc9fe410c
chauveau.alexis@hotmail.fr <92083099+Drakinfer@users.noreply.github.com>
2023-06-23 12:01:10 +0200

    change postRDV

--- controllers/rdv/postRdv.js
+++ controllers/rdv/postRdv.js
@@ -7,10 +7,11 @@ const pool = require('../../config/database');
 module.exports = async (req, res) => {
     let connection;
     const { id } = req.params;
+    console.log(id)
     const { cabinet, animal, date } = req.body;
     try {
         connection = await pool.getConnection();
-        const data = await connection.query('CALL post_rdv(? ,? ,? ,?);', [id, cabinet, animal, date]);
+        const data = await connection.query('CALL post_rdv(? ,? ,? ,?);', [Number(id), cabinet, animal, new Date(date)]);
         return res.status(200).json({success: true, data: data});
     } catch (error) {
         return res.status(400).json({error: error.message});
